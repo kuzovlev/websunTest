74946f9936c57599e74d90418e77256b363756ed
Дмитрий <kuzovlev.dm@gmail.com>
2019-02-26 16:26:12 +0200

    e

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-$(document).ready(function() {
-    $("#start-button").click(function() {
+$(document).ready(function () {
+    $("html, body").animate({
+        scrollTop: $(document).height()
+    }, 3000);
+    $("#start-button").click(function () {
         $(".main").hide();
         $(".questions-page").show();
         $("body").addClass('grey-bg');
     });
-    $('.btn').click(function() {
+    $('.btn').click(function () {
         $(this).closest('.toggleQuestion').hide().next('.toggleQuestion').fadeIn();
     });
 });
